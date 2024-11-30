3a8e26f46e8d0aaba08ec9c5518eac6381e920b0
amanduquia <alejandra.1701914799@ucaldas.edu.co>
2024-11-29 20:16:17 -0500

    cambios en formatos para analisis de codigo

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ def sumar(a, b):
     """Suma dos números."""
     return a + b
 
+
 def restar(a, b):
     """Resta dos números."""
     return a - b
 
+
 def multiplicar(a, b):
     """Multiplica dos números."""
     return a * b
+
 
 def dividir(a, b):
     """Divide dos números."""
--- test_main.py
+++ test_main.py
@@ -1,17 +1,22 @@
 import pytest
 from main import sumar, restar, multiplicar, dividir
 
+
 def test_sumar():
     assert sumar(3, 5) == 8
+
 
 def test_restar():
     assert restar(15, 6) == 9
 
+
 def test_multiplicar():
     assert multiplicar(9, 8) == 72
 
+
 def test_dividir():
     assert dividir(8, 2) == 4
+
 
 def test_dividir_por_cero():
     with pytest.raises(ValueError):
